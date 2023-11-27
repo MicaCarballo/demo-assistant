cf482eb60eed1a0aa7780b030a58086def158b0f
MicaCarballo <111322755+MicaCarballo@users.noreply.github.com>
2023-11-26 19:59:33 -0800

    added route to get nearby hospitals

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#uvicorn main:app 
+# uvicorn main:app
 # uvicorn main:app --reload
 
 # main imports
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Request
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 import openai
 
 # Custom functions imports
-from functions.openai_requests import convert_audio_to_text, get_chat_response
+from functions.openai_requests import convert_audio_to_text, find_nearby_hospitals, get_chat_response, get_location_from_ip
 from functions.database import store_messages, reset_messages
 from functions.tech_to_speech import convert_text_to_speech
 # Initiate app
@@ -21,15 +21,12 @@ origins = [
     "http://localhost:5174",
     "http://localhost:4173",
     "http://localhost:3000",
-    "http://localhost:8000",
-    "https://assistant-backend.onrender.com",
-     "https://assistant-backend.onrender.com/docs"
+    "http://localhost:8000"
 ]
 #Cors- Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    # allow_origins = origins,
+    allow_origins = origins,
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
@@ -98,6 +95,45 @@ async def post_audio(file:UploadFile = File(...)):
    # Return audio file
 
    return StreamingResponse(iterfile(), media_type ="application/octet-stream")   
+
+
+  
+
+# FastAPI route to handle the user's request for nearby hospitals
+# FastAPI route to handle the user's request for nearby hospitals
+@app.get("/nearest-hospitals")
+async def get_nearest_hospitals(request: Request):
+    try:
+        # Use IP-based location
+        user_ip = request.client.host
+        user_location = get_location_from_ip(user_ip)
+
+       # Print user_location for debugging
+        print("User Location:", user_location)
+
+
+        # Check if user_location is valid
+        if not user_location:
+            return {"error": "Invalid user location."}
+
+        # Call the function to find nearby hospitals
+        hospitals = find_nearby_hospitals(user_location)
+
+        if hospitals:
+            # Format the response
+            hospital_names = [hospital["name"] for hospital in hospitals]
+            response_message = f"Here are some nearby hospitals: {', '.join(hospital_names)}"
+        else:
+            response_message = "No hospitals found nearby."
+
+        return {"message": response_message}
+
+    except Exception as e:
+        print(e)
+        return {"error": "Failed to retrieve nearby hospitals."}
+
+# Your existing code...
+
 
 
   
